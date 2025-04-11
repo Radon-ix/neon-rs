8e855784b0d9c5c98296853807868f88c27b6568
Radonoxius <Radonoxius@protonmail.com>
2025-04-11 11:30:10 +0530

    Project restructured.

--- build.sh
+++ build.sh
@@ -11,10 +11,6 @@ rm *.o
 
 cd ..
 
-cp asm/build/libneon.a target/aarch64-unknown-linux-gnu/release/libneon.a
-cp asm/build/libneon.a target/aarch64-unknown-linux-gnu/debug/libneon.a
-cp asm/build/libneon.a target/aarch64-unknown-linux-gnu/release/deps/libneon.a
-cp asm/build/libneon.a target/aarch64-unknown-linux-gnu/debug/deps/libneon.a
 cp asm/build/libneon.a target/release/libneon.a
 cp asm/build/libneon.a target/debug/libneon.a
 cp asm/build/libneon.a target/release/deps/libneon.a
