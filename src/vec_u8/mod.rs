758eda0360aa2a9aa184fe78fe8b7d9b544e27bc
Radonoxius <Radonoxius@protonmail.com>
2025-04-12 15:07:07 +0530

    U8 tests are added to test.rs.

--- src/vec_u8/mod.rs
+++ src/vec_u8/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod test;
+
 use std::ops::Add;
 
 use crate::{ffi::{_add_vecu8_16, _add_vecu8_8}, types::{VecU8_16, VecU8_8}};
@@ -31,33 +34,5 @@ impl Add for VecU8_8 {
         }
 
         result
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use crate::types::{VecU8_16, VecU8_8};
-
-    #[test]
-    fn add_u8() {
-        let v0 = VecU8_16 {
-            data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-        };
-
-        let v1 = VecU8_16 {
-            data: [16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-        };
-
-        assert_eq!([17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17], (v0 + v1).data);
-
-        let v0 = VecU8_8 {
-            data: [1, 2, 3, 4, 5, 6, 7, 8]
-        };
-
-        let v1 = VecU8_8 {
-            data: [8, 7, 6, 5, 4, 3, 2, 1]
-        };
-
-        assert_eq!([9, 9, 9, 9, 9, 9, 9, 9], (v0 + v1).data);
     }
 }
